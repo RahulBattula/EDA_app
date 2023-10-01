a383890d90c6d6ebc62bbe4d6d4970e0b34f3c26
rahulbattula <96605083+RahulBattula@users.noreply.github.com>
2023-10-01 08:11:57 +0530

    Update EDA_app.py

--- EDA_app.py
+++ EDA_app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-from ydata_profiling import ProfileReport
+from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
 st.markdown('''

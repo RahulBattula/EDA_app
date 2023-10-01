0358d73842ae1e001754a49221ffd9340f9da6c0
rahulbattula <96605083+RahulBattula@users.noreply.github.com>
2023-10-01 08:17:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-from pandas_profiling import ProfileReport
 from ydata_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
